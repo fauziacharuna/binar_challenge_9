c793d6898eff0c3fd7c32e4faa18cab14cbee7bb
fauziacharuna <fauziharuna@gmail.com>
2022-03-31 10:24:08 +0800

    fixing router

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/Login/LoginPage";
 import RegisterPage from "./pages/Register/RegisterPage";
+
 const AppRoutes = () => {
-  return (
-    <>
-      <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<LandingPage/>} />
-      </Routes>
-    </>
-  );
+    return (
+                <Routes>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/register" element={<RegisterPage/>}/>
+                    <Route path="/" element={<LandingPage/>}/>
+                </Routes>
+    );
 };
 
 export default AppRoutes;
