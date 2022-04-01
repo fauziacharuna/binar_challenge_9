ab20452faa57e7a2b70dec43db0cc2fae43a5295
fauziacharuna <fauziharuna@gmail.com>
2022-04-01 10:41:31 +0800

    Fixing conflict

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,24 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-=======
-import { Route, Routes } from "react-router-dom";
 import Game from "./pages/Games/Game";
->>>>>>> 7b889862efd07627cd34b7f9c2963e4519cc5b1e
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/Login/LoginPage";
 import RegisterPage from "./pages/Register/RegisterPage";
 
 const AppRoutes = () => {
-<<<<<<< HEAD
-    return (
-                <Routes>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path="/" element={<LandingPage/>}/>
-                </Routes>
-    );
-=======
+
   return (
     <>
       <Routes>
@@ -29,7 +17,6 @@ const AppRoutes = () => {
       </Routes>
     </>
   );
->>>>>>> 7b889862efd07627cd34b7f9c2963e4519cc5b1e
 };
 
 export default AppRoutes;
